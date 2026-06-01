133ad10a6c4dc624cfe766dee90f9382b7ebcc0a
ajfomby <austinfomby@gmail.com>
2026-06-01 12:36:40 -0400

    Changes to be committed: 	new file:   atm_sensor.py 	new file:   camera.py 	new file:   database.py 	new file:   display.py 	new file:   fan.py 	new file:   internal_sensor.py 	new file:   microphone.py 	new file:   temp_warning.py
        -- initial commit with module outlines/notes --

---
+++ display.py
@@ -0,0 +1,7 @@
+"""
+This is a python module for the Smith Lab weather station.
+
+Needs to:
+- Output current temp, humidity, and pressure to the display on the exterior of the box
+- Warning light if parameters go above or below certain thresholds
+"""
