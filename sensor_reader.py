6be14f300895e079f2b8a581f0048f711ebf0b0b
ajfomby <austinfomby@gmail.com>
2026-06-15 12:08:22 -0400

    Changes to be committed: 	modified:   sensor_reader.py         - added inside dome temperature sensing code

--- sensor_reader.py
+++ sensor_reader.py
@@ -1,11 +1,26 @@
+"""
+Needs to read data off the raspberry pi and then disply it to the external temp/humidity display.
+"""
+
+import datetime
+
+import board
+from adafruit_bme280 import basic as adafruit_bme280
+i2c = board.I2C() # uses board.SCL and board.SDA
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+
 def get_weather_data():
     return {
-        "inside": {
-            "temperature": 72.0,
-            "humidity": 40.0,
+        "inside dome": {
+            "temperature": bme280.temperature,
+            "humidity": bme280.humidity
         },
-        "outside": {
-            "temperature": 65.0,
-            "humidity": 55.0
+        "outside dome": {
+            "temperature": None,    # need to communicate with other group to get measurements from outside weather station for both of these
+            "humidity": None
+        },
+        "time": {
+            "Date": datetime.date,
+            "Time": datetime.time
         }
     }
