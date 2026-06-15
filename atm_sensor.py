5b7dd9afdd012fa9ad794d456d7b00a4fa819153
ajfomby <austinfomby@gmail.com>
2026-06-15 12:19:56 -0400

    Changes to be committed: 	deleted:    atm_sensor.py         -unneeded

--- atm_sensor.py
+++
@@ -1,7 +0,0 @@
-"""
-Python module for the atmospheric sensor on the Smith Lab weather station.
-Needs to:
-
-- Update the website with the current atmospheric pressure, temperature, and humidity every [insert time period here]
-- Possibly add functionality to send alerts if the atmospheric pressure, temperature, or humidity goes above or below certain thresholds
-"""
