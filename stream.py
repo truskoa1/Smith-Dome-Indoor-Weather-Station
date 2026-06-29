39d28b7be5995da4f81a8a74a8587ab7c08b03b3
truskoa1 <truskoa1@gmail.com>
2026-06-29 12:07:24 -0400

    debugged indentation

--- stream.py
+++ stream.py
@@ -13,15 +13,15 @@ app = Flask(__name__)
 
 def generate_frames():
     with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    camera.framerate = 24
-    stream = io.BytesIO()
+        camera.resolution = (640, 480)
+        camera.framerate = 24
+        stream = io.BytesIO()
 
-    for _ in camera.capture_continuous(stream,'jpeg', use_video_port=True):
-        stream.seek(0)
-        yield b'--frame \r\nContent-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n'
-        stream.seek(0)
-        stream.truncate()
+        for _ in camera.capture_continuous(stream,'jpeg', use_video_port=True):
+            stream.seek(0)
+            yield b'--frame \r\nContent-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n'
+            stream.seek(0)
+            stream.truncate()
 
 @app.route('/video_feed')
 def video_feed():
